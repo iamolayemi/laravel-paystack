9923eddf3b52f963d6e3b7c6ceede99ebb9eaf45
dybaay <71703569+dybaay@users.noreply.github.com>
2024-04-16 14:06:00 +0100

    comments and little refactoring

--- src/Endpoints/Endpoint.php
+++ src/Endpoints/Endpoint.php
@@ -2,63 +2,64 @@
 
 namespace Iamolayemi\Paystack\Endpoints;
 
-use Exception;
+use Exceptions\PaystackConnectionException;
 use Iamolayemi\Paystack\Paystack;
 use Illuminate\Http\Client\Response;
 
 class Endpoint
 {
-    protected const BASE_URL = 'https://api.paystack.co';
+    protected const BASE_URL = 'https://api.paystack.co/';
 
     public Paystack $paystack;
 
     protected ?Response $response = null;
 
     /**
-     * @throws Exception
+     * @throws PaystackConnectionException
      */
     public function __construct(Paystack $paystack)
     {
         $this->paystack = $paystack;
 
         if ($this->paystack->getConnection() === null) {
-            throw new Exception('Connection could not be established.');
+            throw new PaystackConnectionException('Connection could not be established.');
         }
     }
 
     /**
      * Return the data from http request.
+     * @return array|null The data from the response, or null if no request has been made.
      */
-    public function data(): array
+    public function data(): ?array
     {
-        return $this->response('data');
+        return $this->response ? $this->response('data') : null;
     }
 
     /**
      * Return the response from http request.
-     *
-     * @return array
+     * @param  string  $key  Optional key to extract a specific value from the JSON response.
+     * @return array|mixed The full response or a specific value from the response.
      */
     public function response(string $key = ''): ?array
     {
-        if (! empty($key)) {
-            return $this->response->json($key);
-        } else {
-            return $this->response->json();
+        if (empty($key)) {
+            return $this->response?->json();
         }
+        return $this->response?->json($key);
     }
 
     protected function url(string $endpoint): string
     {
-        return Endpoint::BASE_URL."$endpoint";
+        return Endpoint::BASE_URL.ltrim($endpoint, '/');
     }
 
     /**
      *  * Make a http get request
-     *
-     * @param  array|string|null  $query
+     * @param  string  $url  The URL to send the request to.
+     * @param  array|string|null  $query  Optional query string parameters.
+     * @return self This instance for method chaining.
      */
-    protected function get(string $url, $query = []): self
+    protected function get(string $url, array|string|null $query = []): self
     {
         $response = $this->paystack->getConnection()->get($url, $query);
 
@@ -69,6 +70,9 @@ class Endpoint
 
     /**
      * Make a http post request
+     * @param  string  $url  The URL to send the request to.
+     * @param  array  $payload  The data to be sent.
+     * @return self This instance for method chaining.
      */
     protected function post(string $url, array $payload = []): self
     {
@@ -81,6 +85,9 @@ class Endpoint
 
     /**
      * Make a http put request
+     * @param  string  $url  The URL to send the request to.
+     * @param  array  $payload  The data to be sent.
+     * @return self This instance for method chaining.
      */
     protected function put(string $url, array $payload = []): self
     {
@@ -92,7 +99,10 @@ class Endpoint
     }
 
     /**
-     * Make a http put request
+     * Make a http delete request
+     * @param  string  $url  The URL to send the request to.
+     * @param  array  $payload  The data to be sent.
+     * @return self This instance for method chaining.
      */
     protected function delete(string $url, array $payload = []): self
     {
